e08b51ebdd0aaec9f67ecb776cfef0e59bd7d6c7
Vladislav <118668125+drxxmy@users.noreply.github.com>
2025-06-07 02:28:04 +0500

    Create a basic API for managing bookmarks

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
-def main():
-    print("Hello from osu-bookmarks-api!")
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import datetime as dt
 
 
-if __name__ == "__main__":
-    main()
+class Beatmap(BaseModel):
+    map_id: int
+    map_name: str
+    map_creator: str
+    tags: list[str]
+
+
+class Bookmark(BaseModel):
+    beatmap: Beatmap
+    date_added: dt.datetime
+
+
+class User(BaseModel):
+    name: str
+    bookmarks: list[Bookmark]
+
+
+users: list[User] = []
+test_user = User(name="Test", bookmarks=[])
+users.append(test_user)
+
+app = FastAPI()
+
+
+@app.get("/api/v1/users/{user_id}/bookmarks")
+async def list_bookmarks(user_id: int, limit: int = 25):
+    return users[user_id].bookmarks[0:limit]
+
+
+@app.post("/api/v1/users/{user_id}/bookmarks")
+async def create_bookmark(user_id: int, bookmark: Bookmark):
+    users[user_id].bookmarks.append(bookmark)
+    return users[user_id].bookmarks
+
+
+@app.get("/api/v1/users/{user_id}/bookmarks/{bookmark_id}")
+async def get_bookmark(user_id: int, bookmark_id: int) -> Bookmark:
+    if bookmark_id < len(users[user_id].bookmarks):
+        return users[user_id].bookmarks[bookmark_id]
+    else:
+        raise HTTPException(
+            404,
+            f"Cannot use id: '{bookmark_id}' since it is out of range!",
+        )
