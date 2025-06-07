dfe3efc7daf9b0caf831fb02e9c747ca7d0158c9
Vladislav <118668125+drxxmy@users.noreply.github.com>
2025-06-07 23:12:14 +0500

    Add creation time field for collections

--- app/models/collection.py
+++ app/models/collection.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from models.beatmap import Beatmap
+import datetime as dt
 
 
 class Collection(BaseModel):
     name: str
     beatmaps: list[Beatmap]
+    created_at: dt.datetime = Field(default_factory=dt.datetime.now)
