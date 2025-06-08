9da2c71363042ebb87ad6bf9a88ed58714d64fc5
Vladislav <118668125+drxxmy@users.noreply.github.com>
2025-06-08 16:16:34 +0500

    Add metadata for the application

--- app/main.py
+++ app/main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 from routers import beatmaps, collections, users
 
 
-app = FastAPI()
+app = FastAPI(
+    title="osu!bookmarks API",
+    version="0.1.0",
+    openapi_url="/api/v1/openapi.json",
+    docs_url="/api/v1/docs",
+)
 
 app.include_router(beatmaps.router)
 app.include_router(collections.router)
